78e5559c7610cb99308dd2bbfc48ca97644e3830
Ekaterina Pustovaya <krobox@gmail.com>
2017-05-10 00:03:41 +0300

    main pages

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -7,6 +7,9 @@ module.exports = function() {
       .pipe($.gp.sassGlob())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
+      .pipe($.gp.cssUnit({
+        type: 'px-to-rem'
+      }))
       .pipe($.gp.sourcemaps.write())
       .pipe($.gulp.dest($.config.root + '/assets/css'))
       .pipe($.browserSync.stream());
